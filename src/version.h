0bd9f5f6c8f06ae6066e1e323ad7da3b632aa2f8
AlvajoyAsante <alvajoy123@gmail.com>
2026-08-13 22:26:20 -0400

    Update application version to a0.1.0 in version.h

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 #define APP_TITLE "Little Alchemy"
-#define APP_VERSION "b1.0"
+#define APP_VERSION "a0.1.0"
 #define APP_TITLE_WITH_VERSION APP_TITLE " (" APP_VERSION ")"
 
 #endif
